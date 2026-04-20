aac411b918eea081e9c3a4b1229cc717e41dfbd8
sanjay-TN <sanjaysanjaytn77@gmail.com>
2026-04-20 19:39:01 +0530

    added javascript datatypes

--- Javascript/02datatypes.js
+++ Javascript/02datatypes.js
@@ -31,3 +31,42 @@ console.log(typeof address); // Output: undefined
 console.log(typeof phoneNumber); // Output: object (null is considered an object in JavaScript)
 console.log(typeof uniqueId); // Output: symbol
 console.log(typeof bigNumber); // Output: bigint
+
+// 2. Non-primitive data types (objects)
+// a. Object: A collection of properties, where each property is a key-value pair
+let person = {
+  name: "Alice",
+  age: 25,
+  isStudent: false,
+};
+console.log(typeof person); // Output: object
+
+// b. Array: A special type of object used to store ordered collections of values
+let numbers = [1, 2, 3, 4, 5];
+console.log(typeof numbers); // Output: object (arrays are also considered objects in JavaScript)
+
+// c. Function: A block of code designed to perform a particular task
+function greet() {
+  console.log("Hello, World!");
+}
+console.log(typeof greet); // Output: function
+
+// d. Date: A built-in object that represents a single moment in time
+let currentDate = new Date();
+console.log(typeof currentDate); // Output: object
+
+// e. RegExp: A built-in object that provides a way to work with regular expressions
+let regex = /ab+c/;
+console.log(typeof regex); // Output: object
+
+// f. Map: A collection of key-value pairs where keys can be of any type
+let map = new Map();
+map.set("key1", "value1");
+console.log(typeof map); // Output: object
+
+// g. Set: A collection of unique values
+let set = new Set();
+set.add(1);
+set.add(2);
+set.add(2);
+console.log(typeof set); // Output: object
