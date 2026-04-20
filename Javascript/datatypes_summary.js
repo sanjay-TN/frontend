92f6c004e92cc4eb9bf351adaf985bb5467beda4
sanjay-TN <sanjaysanjaytn77@gmail.com>
2026-04-20 21:43:12 +0530

    added summary about datatypes in js

---
+++ Javascript/datatypes_summary.js
@@ -0,0 +1,27 @@
+// types of data in JavaScript
+
+// 1. Primitive data types
+// it has 7 types of primitive data types
+
+// 1. String : a sequence of characters enclosed in single quotes, double quotes, or backticks.
+
+// 2. Number : a numeric data type that can be an integer or a float.
+
+// 3. Boolean : a logical data type that can have only two values: true or false.
+
+// 4. Null : a special value that represents the absence of any object value.
+
+// 5. Undefined : a special value that represents the absence of a value.
+
+// 6. Symbol : a unique identifier that is often used as a key for object properties.
+
+// 7. BigInt : a data type that can represent integers of arbitrary size.
+
+// 2. Non-primitive data types (also known as reference data types)
+// it has Array, Objects, Functions type of non-primitive data type
+
+// 1. Array : a collection of values that can be of any data type, enclosed in square brackets and separated by commas.
+
+// 2. Object : a collection of key-value pairs, where the keys are strings and the values can be of any data type, enclosed in curly braces.
+
+// 3. Function : a block of code that can be executed when called, defined using the function keyword or as an arrow function.
